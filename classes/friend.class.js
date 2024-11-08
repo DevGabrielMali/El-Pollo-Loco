bf34a0cd4180bedbe0ec79951cd2fa6a3c4e6816
Gabriel Mali <gabriel.mali@outlook.de>
2024-11-08 17:50:44 +0100

    delete practices and official start of the pollos loco project

--- classes/friend.class.js
+++
@@ -1,9 +0,0 @@
-// die class Friend bedient sich aus der "Mutter"-class Person
-class Friend extends Person {
-    befreundetSeit;
-
-    constructor(firstNameParam, lastNameParam) {
-        super(firstNameParam, lastNameParam);
-    }
-}
-

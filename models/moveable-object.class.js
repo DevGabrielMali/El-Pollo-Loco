0e99c4d50c8f36c53a5eb3b8d39baba42fd25c06
Gabriel Mali <gabriel.mali@outlook.de>
2024-11-13 18:19:10 +0100

    load images prepare

--- models/moveable-object.class.js
+++ models/moveable-object.class.js
@@ -4,11 +4,20 @@ class MoveableObject {
     img;
     height = 400;
     width = 200;
+    imageCache = [];
 
     loadImage(path) {
         this.img = new Image(); // Image ist ein bestehendes element in JavaScript. gleichzusetzen mit dem <img id="image"> + this.img = document.getElementById('image')
         this.img.src = path;
     }
+
+    loadImages(arr) {
+        arr.forEach((path) => {
+        let img = new Image();
+        img.src = path;
+        this.imageCache[path] = path;
+    });
+    } 
 
     moveRight() {
         console.log('Move Bitch');
@@ -18,4 +27,4 @@ class MoveableObject {
     moveLeft() {
         
     }
-}
+} 

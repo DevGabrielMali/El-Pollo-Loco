1444f79a30a2df798a3653675330236026291196
Gabriel Mali <gabriel.mali@outlook.de>
2024-11-13 21:11:32 +0100

    animated main character
    setInterval and animations

--- models/moveable-object.class.js
+++ models/moveable-object.class.js
@@ -1,30 +1,31 @@
 class MoveableObject {
-    x = 100;
-    y = 250;
-    img;
-    height = 400;
-    width = 200;
-    imageCache = [];
+  x = 100;
+  y = 250;
+  img;
+  height = 400;
+  width = 200;
+  imageCache = {};
 
-    loadImage(path) {
-        this.img = new Image(); // Image ist ein bestehendes element in JavaScript. gleichzusetzen mit dem <img id="image"> + this.img = document.getElementById('image')
-        this.img.src = path;
-    }
+  loadImage(path) {
+    this.img = new Image(); // Image ist ein bestehendes element in JavaScript. gleichzusetzen mit dem <img id="image"> + this.img = document.getElementById('image')
+    this.img.src = path;
+  }
 
-    loadImages(arr) {
-        arr.forEach((path) => {
-        let img = new Image();
-        img.src = path;
-        this.imageCache[path] = path;
+  /**
+   * 
+   * @param {Array} arr - ['img/image1.png','img/image2.png','img/image3.png'] 
+   */
+  loadImages(arr) { // arr ist unser parameter in dem ein Array eingebunden ist (in der character.class)
+    arr.forEach((path) => { // wir gehen jedes element (forEach) im array durch. path steht hier für den tatsächlichen bildpfad
+      let img = new Image(); // Image ist ein bestehendes element in JavaScript. gleichzusetzen mit dem <img id="image"> + this.img = document.getElementById('image')
+      img.src = path; // die img quelle ist der bildpfad des aktuellen bildes
+      this.imageCache[path] = img; 
     });
-    } 
+  }
 
-    moveRight() {
-        console.log('Move Bitch');
-    }
+  moveRight() {
+    console.log("Move Bitch");
+  }
 
-
-    moveLeft() {
-        
-    }
-} 
+  moveLeft() {}
+}

01d3c402f0ca955b34ebc872bfac9bcf5cad9d29
Gabriel Mali <gabriel.mali@outlook.de>
2024-11-15 17:01:34 +0100

    Step sounds and endboss v1

--- models/character.class.js
+++ models/character.class.js
@@ -11,6 +11,7 @@ class Character extends MoveableObject {
     "img/2_character_pepe/2_walk/W-26.png",
   ];
   world;
+  walking_sound = new Audio('audio/step.mp3');
 
   constructor() {
     super().loadImage("img/2_character_pepe/2_walk/W-21.png");
@@ -27,14 +28,18 @@ class Character extends MoveableObject {
         if (this.world.keyboard.RIGHT && this.x < this.world.level.level_end_x) {
             this.x += this.speed
             this.otherDirection = false;
+            this.walking_sound.play();
+            this.walking_sound.playbackRate = this.speed / 1.5; // Passe die Geschwindigkeit an
         }
 
         if (this.world.keyboard.LEFT && this.x > 0) {
             this.x -= this.speed
             this.otherDirection = true;
+            this.walking_sound.play();
+            this.walking_sound.playbackRate = this.speed / 1.5; // Passe die Geschwindigkeit an
         }
         this.world.camera_x = - this.x + 100;
-    }, 500 / 60);
+    }, 1000 / 60);
 
 
     setInterval(() => {

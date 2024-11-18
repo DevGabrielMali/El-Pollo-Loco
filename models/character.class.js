f34eda99c8cd425b98f3b6550a24a85f9c64d417
Gabriel Mali <gabriel.mali@outlook.de>
2024-11-18 11:52:02 +0100

    push

--- models/character.class.js
+++ models/character.class.js
@@ -1,28 +1,53 @@
 class Character extends MoveableObject {
 
     speed = 5;
-
+    y = 100;
   IMAGES_WALKING = [
     "img/2_character_pepe/2_walk/W-21.png",
     "img/2_character_pepe/2_walk/W-22.png",
     "img/2_character_pepe/2_walk/W-23.png",
     "img/2_character_pepe/2_walk/W-24.png",
     "img/2_character_pepe/2_walk/W-25.png",
-    "img/2_character_pepe/2_walk/W-26.png",
+    "img/2_character_pepe/2_walk/W-26.png"
   ];
+
+
+  IMAGES_JUMPING = [
+    'img/2_character_pepe/3_jump/J-31.png',
+    'img/2_character_pepe/3_jump/J-32.png',
+    'img/2_character_pepe/3_jump/J-33.png',
+    'img/2_character_pepe/3_jump/J-34.png',
+    'img/2_character_pepe/3_jump/J-35.png',
+    'img/2_character_pepe/3_jump/J-36.png',
+    'img/2_character_pepe/3_jump/J-37.png',
+    'img/2_character_pepe/3_jump/J-38.png',
+    'img/2_character_pepe/3_jump/J-39.png'
+  ];
+
+
+  IMAGES_HURT = [
+    'img/2_character_pepe/4_hurt/H-41.png',
+    'img/2_character_pepe/4_hurt/H-42.png',
+    'img/2_character_pepe/4_hurt/H-43.png'
+  ]
   world;
   walking_sound = new Audio('audio/step.mp3');
 
   constructor() {
     super().loadImage("img/2_character_pepe/2_walk/W-21.png");
     this.loadImages(this.IMAGES_WALKING);
-
+    this.loadImages(this.IMAGES_JUMPING);
+    this.applyGravity()
     this.animate();
   }
 
   animate() {
 
     setInterval(() => {
+
+      if (this.isAboveGround()) {
+        this.playAnimation(this.IMAGES_JUMPING)
+      } else
 
         // hiermit bewegt sich unser character
         if (this.world.keyboard.RIGHT && this.x < this.world.level.level_end_x) {
--- models/moveable-object.class.js
+++ models/moveable-object.class.js
@@ -8,6 +8,21 @@ class MoveableObject {
   currentImage = 0;
   speed = 0.2;
   otherDirection = false;
+  speedY = 0;
+  acceleration = 3;
+
+  applyGravity() {
+    setInterval(() => {
+      if (this.isAboveGround()) {
+        this.y -= this.speedY;
+        this.speedY -= this.acceleration;
+      }
+    }, 500 / 25);
+  }
+
+  isAboveGround(){
+    return this.y < 250;
+  }
 
   loadImage(path) {
     this.img = new Image(); // Image ist ein bestehendes element in JavaScript. gleichzusetzen mit dem <img id="image"> + this.img = document.getElementById('image')
@@ -15,23 +30,25 @@ class MoveableObject {
   }
 
   /**
-   * 
-   * @param {Array} arr - ['img/image1.png','img/image2.png','img/image3.png'] 
+   *
+   * @param {Array} arr - ['img/image1.png','img/image2.png','img/image3.png']
    */
-  loadImages(arr) { // arr ist unser parameter in dem ein Array eingebunden ist (in der character.class)
-    arr.forEach((path) => { // wir gehen jedes element (forEach) im array durch. path steht hier für den tatsächlichen bildpfad
+  loadImages(arr) {
+    // arr ist unser parameter in dem ein Array eingebunden ist (in der character.class)
+    arr.forEach((path) => {
+      // wir gehen jedes element (forEach) im array durch. path steht hier für den tatsächlichen bildpfad
       let img = new Image(); // Image ist ein bestehendes element in JavaScript. gleichzusetzen mit dem <img id="image"> + this.img = document.getElementById('image')
       img.src = path; // die img quelle ist der bildpfad des aktuellen bildes
-      this.imageCache[path] = img; 
+      this.imageCache[path] = img;
     });
   }
 
-  playAnimation(images){
-            // hier wird das laufen animiert (ist aber noch an der selben x stelle fixiert)
-            let i = this.currentImage % this.IMAGES_WALKING.length; // % funktioniert ähnlich wie ein loop in kombination mit xxx.length
-            let path = images[i];
-            this.img = this.imageCache[path];
-            this.currentImage++;
+  playAnimation(images) {
+    // hier wird das laufen animiert (ist aber noch an der selben x stelle fixiert)
+    let i = this.currentImage % this.IMAGES_WALKING.length; // % funktioniert ähnlich wie ein loop in kombination mit xxx.length
+    let path = images[i];
+    this.img = this.imageCache[path];
+    this.currentImage++;
   }
 
   moveRight() {
@@ -39,8 +56,8 @@ class MoveableObject {
   }
 
   moveLeft() {
-    setInterval( () => {
-        this.x -= this.speed; // Bestimmt das Tempo der Wolken
+    setInterval(() => {
+      this.x -= this.speed; // Bestimmt das Tempo der Wolken
     }, 1000 / 60); // 60 Frames pro Sekunde (1000 = 1 sek)
-}
+  }
 }
